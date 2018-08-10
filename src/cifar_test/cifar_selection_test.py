351768f57f07d294439a46d01d9cf21b0ffe7e91
ssilv <18516053895@163.com>
2018-08-10 20:21:03 +0800

    cifar_selection

--- src/cifar_test/cifar_selection_test.py
+++ src/cifar_test/cifar_selection_test.py
@@ -28,13 +28,61 @@ def train():
     values = y
     n_values = 10
     y = np.eye(n_values)[values]
-    cnn_profiler.train([None, 32, 32, 3], [None, 10], x, y, iter=6000)
+    cnn_profiler.train([None, 32, 32, 3], [None, 10], x, y, iter=2500)
 ################## Test ########################
 def test():
-    x = X_train.astype("float32")
-    y = y_train.reshape(-1)
+    x = X_test.astype("float32")
+    y = y_test.reshape(-1)
     values = y
     n_values = 10
     y = np.eye(n_values)[values]
     cnn_profiler.test([None, 32, 32, 3], [None, 10], x[:2000], y[:2000])
-train()
+def get_distribution(label, anchor, filter):
+    x = X_train.astype("float32")
+    y = y_train.reshape(-1)
+    x = x[y == label]
+    y = y[y == label]
+    values = y
+    n_values = 10
+    y = np.eye(n_values)[values]
+    correct = cnn_profiler.get_correct_mid([None, 32, 32, 3], [None, 10], x, y, anchor=anchor, filter=filter)
+    distribute_correct = Distribution(correct)
+    return distribute_correct
+def calc_distribution(anchor, filter):
+    distribution_list = []
+    for i in range(10):
+        distribution_list.append(get_distribution(i, anchor, filter))
+    return distribution_list
+def test_m_dist(label):
+    x = X_train.astype("float32")
+    y = y_train.reshape(-1)
+    x = x[y == label]
+    y = y[y == label]
+    values = y
+    n_values = 10
+    y = np.eye(n_values)[values]
+    anchor = -2
+    filter = None
+    test_correct_dis = []
+    correct_test = cnn_profiler.get_correct_mid([None, 32, 32, 3], [None, 10], x, y, anchor=anchor, filter=filter)
+    wrong_test = cnn_profiler.get_correct_mid([None, 32, 32, 3], [None, 10], x, y, wrong=True, anchor=anchor, filter=filter)
+    distribution_list = calc_distribution(anchor, filter)
+    for j in range(10):
+        distribute_correct = distribution_list[j]
+        for i in range(correct_test.shape[0]):
+            dis = distribute_correct.mahanobis_distance(correct_test[i])
+            test_correct_dis.append(dis)
+        test_correct_dis = np.array(test_correct_dis)
+        test_wrong_dis = []
+        for i in range(wrong_test.shape[0]):
+            dis = distribute_correct.mahanobis_distance(wrong_test[i])
+            test_wrong_dis.append(dis)
+        test_wrong_dis = np.array(test_wrong_dis)
+        print("############# to label %d ##############"%j)
+        print(test_correct_dis)
+        print(test_correct_dis.max())
+        print(test_correct_dis.mean())
+        print(test_wrong_dis)
+        print(test_wrong_dis.max())
+        print(test_wrong_dis.mean())
+test_m_dist(3)
